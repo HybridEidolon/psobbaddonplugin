51b9f14e470a168ae4329cc99a93874ea63ef72f
Ronald Kinard <furyhunter600@gmail.com>
2017-05-05 18:04:08 -0500

    Add exit game button

--- addons/core_mainmenu/init.lua
+++ addons/core_mainmenu/init.lua
@@ -10,6 +10,8 @@ local remove_button
 
 local window_open = true
 
+local exit_game_window = false
+
 init = function()
   return {
     name = "Core - Main Menu",
@@ -33,6 +35,7 @@ present = function()
     return
   end
   local status
+  imgui.SetNextWindowSize(200, 250, 'FirstUseEver')
   status, window_open = imgui.Begin('Main', window_open)
   for name, func in pairs(hooked_buttons) do
     if (imgui.Button(name)) then
@@ -42,7 +45,27 @@ present = function()
   if (imgui.Button('Reload')) then
     pso.reload()
   end
+  if (imgui.Button('Exit Game')) then
+    exit_game_window = true
+  end
   imgui.End()
+
+  if (exit_game_window) then
+    status, exit_game_window = imgui.Begin('Exit Game', exit_game_window)
+    if (exit_game_window) then
+      imgui.Text('Are you sure?')
+      if (imgui.Button('Yes')) then
+        os.exit()
+      end
+      imgui.SameLine()
+      if (imgui.Button('No')) then
+        exit_game_window = false
+      end
+      imgui.End()
+    else
+      imgui.End()
+    end
+  end
 end
 
 add_button = function(name, func)
