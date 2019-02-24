dd24be2ed47bebf0a126a7df4d28847b14e957b3
Elixir70 <43532606+Elixir70@users.noreply.github.com>
2019-02-24 14:39:34 -0500

    Adding 'Settings Editor' addon used to set and change the global font and language.

---
+++ addons/Settings Editor/options.lua
@@ -0,0 +1,15 @@
+return
+{
+noResize = "",
+fontScale = 1,
+noMove = "",
+useCustomTheme = false,
+refreshTimer = 1,
+fontSize = 13,
+oversampleV = 1,
+noTitleBar = "",
+oversampleH = 1,
+transparentWindow = false,
+fontName = "ProggyClean (Default).ttf",
+language = "EN",
+}
