c10ed0840d510d2bdfd1839044fc3f0e03d9d80a
Elixir70 <43532606+Elixir70@users.noreply.github.com>
2019-03-04 16:26:15 -0500

    Updated Settings Editor addon to use a Save button instead of dynamically changing settings and added options for selecting a second font for merging.

--- addons/Settings Editor/init.lua
+++ addons/Settings Editor/init.lua
@@ -1,4 +1,4 @@
--- Settings Editor - Addon that can change global font face, font size, addon language (EN/JP).
+﻿-- Settings Editor - Addon that can change global font face, font size, addon language (EN/JP).
 --                   There are other global changes involved when changing the font face and size.
 
 local core_mainmenu                    = require("core_mainmenu")
@@ -8,31 +8,44 @@ local optionsLoaded, options           = pcall(require, "Settings Editor.options
 local addonName                        = "Settings Editor"
 local addonHome                        = "addons/" .. addonName .. "/"
 local optionsFileName                  = addonHome .. "options.lua"
+local MIN_FONT_SIZE = 4
+local MAX_FONT_SIZE = 48
 
 local selectedFont                     -- index into customFontList
+local selectedFontCJKMerge             -- index into customFontList for the CJK merge font
 local customFontList                   -- list of fonts
 local numFonts                         -- should be equal to table.getn(customFontList)
 local display                          = false
-local globalOptionsChanged             = false -- all addons 
-local addonOptionsChanged              = false -- just this addon
 
 
--- field, default
+
+ -- field, default
 local _SettingsEditorDefaults = {
-    {"language", "EN"},
-    {"fontSize", 13},
-    {"fontName", "ProggyClean (Default).ttf"},
-    {"oversampleH", 1},
-    {"oversampleV", 1},
-    {"useCustomTheme", false},
-    {"fontScale", 1.0},
+    {"language", "EN"},                        -- Current language so that it can be saved globally
+    {"fontSize", 13},                          -- fontSize for main font
+    {"fontSizeCJKMerge", 13},                  -- Chinese/Japanese/Korean glyph range font size if merging is on
+    {"mergeFonts", false},                     -- merge the two fonts (fontName main, fontNameCJKMerge for CJK glyphs)
+    {"fontName", "ProggyClean (Default).ttf"}, -- font used for basic Latin (and CJK if mergeFonts == false)
+    {"fontNameCJKMerge", ""},                  -- font used for CJK glyph range only
+    {"oversampleH", 1},                        -- be careful with this--can cause a big memory spike for some fonts
+    {"oversampleV", 1},                        -- not actually used by imgui, but will remain here
+    {"useCustomTheme", false},                 -- custom theme or not
     {"noResize", ""},
     {"noTitleBar", ""},
     {"noMove", ""},
     {"transparentWindow", false},
-    {"refreshTimer", 0},
+    {"fontScale", 1.0},
 }
 
+-- Return value restricted to the range
+local function RestrictOptionsRange(val, min, max)
+    if val < min then
+        return min
+    elseif val > max then
+        return max
+    end
+    return val
+end
 -- Debugging
 local function PrintOptions()
     for key, val in pairs(options) do
@@ -40,7 +53,6 @@ local function PrintOptions()
     end
 end
 
--- Write one level of table to a file
 local function SaveTableToFile(tbl, fileName)
     local file = io.open(fileName, "w")
     if file ~= nil then
@@ -52,15 +64,15 @@ local function SaveTableToFile(tbl, fileName)
             local ktype = type(key)			
             local sval
             local vtype = type(val)
-                       
+            
             -- Quote or convert if necessary. No table handling here because
             -- options table isn't nested (yet).
             if     vtype == "string"  then sval = string.format("%q", val)
             elseif vtype == "number"  then sval = string.format("%s", val)
             elseif vtype == "boolean" then sval = tostring(val) 
             end
-                        
-            io.write(string.format("%s = %s,\n", key, sval))
+            
+            io.write(string.format("\t%s = %s,\n", key, sval))
         end
         io.write("}\n")
         io.close(file)
@@ -68,29 +80,25 @@ local function SaveTableToFile(tbl, fileName)
 end
 
 local function SaveOptions(tbl, fileName)
+    --print(addonName, "Saving Options")
     SaveTableToFile(tbl, fileName)	
 end
 
--- Setup the options table 
-if optionsLoaded then
-    for _, opt in pairs(_SettingsEditorDefaults) do
-        options[opt[1]] = lib_helpers.NotNilOrDefault(options[opt[1]], opt[2])
-    end
-    pso.set_language(options.language)
-else
-    -- Not loaded, so generate and save it here
-    options = {}
-    for _, opt in pairs(_SettingsEditorDefaults) do
-        options[opt[1]] = opt[2]
-    end
-        
-    -- We just created the options, so we should save to have valid file
-    SaveOptions(options, optionsFileName) 
+local function SaveThisAddonsOptions()
+    SaveOptions(options, optionsFileName)
+end
+
+local function SetAddonLanguage(lang)
+     pso.set_language(lang)
 end
 
 -- Call the plugin function to prepare next frame
 local function ChangeGlobalFont()
-    pso.change_global_font("./" .. addonHome .. "/" .. options.fontName, options.fontSize, options.oversampleH, options.oversampleV)
+    local fontName1 = "./" .. addonHome .. "/" .. options.fontName
+    local fontName2 = "./" .. addonHome .. "/" .. options.fontNameCJKMerge
+
+    -- Need paths to the font files from the ROOT PSO directory
+    pso.change_global_font(fontName1, options.fontSize, options.oversampleH, options.oversampleV, options.mergeFonts, fontName2, options.fontSizeCJKMerge)
 end
 
 local function GetFileExtension(file)
@@ -101,25 +109,34 @@ local function CheckFileExtension(file, ext)
     return GetFileExtension(file) == ext
 end
 
+-- Load the font list. Supports only ttf for now because otf files 
+-- don't work in imgui
 local function LoadFontList()
     customFontList = {}
     numFonts = 0
     selectedFont = 1
-        
+    selectedFontCJKMerge = 1
+    
     local filesInThisDir = pso.list_directory_files(addonName)
     for _, file in pairs(filesInThisDir) do
-        -- Only care about ttf and otf, nothing else.
-        if CheckFileExtension(file, ".ttf") or CheckFileExtension(file, ".otf") then
+        -- Only care about ttf... Seems otf doesn't work in imgui
+        if CheckFileExtension(file, ".ttf") then
             -- Add to table, increment the count, and check if this the saved font
             table.insert(customFontList, file)
             numFonts = numFonts + 1
+
+            -- Check if this is one of the font combobox selections
             if options.fontName == file then
                 selectedFont = numFonts
+            end
+            if options.fontNameCJKMerge == file then
+                selectedFontCJKMerge = numFonts
             end
         end
     end
 end
 
+-- Settings used by every addon (language, font)
 local function PresentGlobalSettings()
     local success
     local oversampleH -- imgui limits this (1,7)
@@ -128,164 +145,174 @@ local function PresentGlobalSettings()
     local currLang = pso.get_language()
     local languageIndex = 1 -- default to EN which is first in the table
     local fontSize
+    local fontSizeCJKMerge
     local count = 1
-        
+    
     for _,str in pairs(languageTable) do
         if str == currLang then
             languageIndex = count
         end
         count = count + 1
     end
-        
+    
     imgui.PushItemWidth(imgui.GetWindowWidth() * 0.25)
     success, languageIndex = imgui.Combo("Language", languageIndex, languageTable, table.getn(languageTable))
     if success then
-        pso.set_language(languageTable[languageIndex])
-        options.language = pso.get_language() 
-        globalOptionsChanged = true
+        SetAddonLanguage(languageTable[languageIndex])
+        options.language = pso.get_language()
+        SaveThisAddonsOptions()
     end
     imgui.PopItemWidth()
-        
-    if imgui.TreeNodeEx("Custom Font Settings", "DefaultOpen") then
+    
+    if imgui.TreeNodeEx("Global Font Settings", "DefaultOpen") then
+
+        -- Main font
         imgui.PushItemWidth(imgui.GetWindowWidth() * 0.50)
         success, selectedFont = imgui.Combo("Font", selectedFont, customFontList, numFonts)
         if success then
             options.fontName = customFontList[selectedFont]
-            globalOptionsChanged = true
         end		
         imgui.PopItemWidth()
-                
+        
+
+        -- Main font size
         imgui.PushItemWidth(imgui.GetWindowWidth() * 0.25)
         success, fontSize = imgui.InputFloat("Custom Font Size", options.fontSize)
-        if success and fontSize > 0 then
-            options.fontSize = fontSize
-            globalOptionsChanged = true
-        end
-                
-        success, oversampleH = imgui.InputInt("Custom Font Oversampling (Horizontal)", options.oversampleH)
-        if success and oversampleH > 0 and oversampleH < 8 then
-            options.oversampleH = oversampleH
-            globalOptionsChanged = true
-        end
-                
-        success, oversampleV = imgui.InputInt("Custom Font Oversampling (Vertical)", options.oversampleV)
-        if success and oversampleV > 0 and oversampleV < 8 then
-            options.oversampleV = oversampleV
-            globalOptionsChanged = true
+        if success then
+            options.fontSize = RestrictOptionsRange(fontSize, MIN_FONT_SIZE, MAX_FONT_SIZE)
         end
         imgui.PopItemWidth()
+
+        -- Do we want a second font?
+        if imgui.Checkbox("Merge Second Font?", options.mergeFonts) then
+            options.mergeFonts = not options.mergeFonts
+        end
+
+        -- If second font is selected...
+        if imgui.TreeNodeEx("Second Font Options") then
+            imgui.PushItemWidth(imgui.GetWindowWidth() * 0.50)
+            success, selectedFontCJKMerge = imgui.Combo("Second Font (CJK Range)", selectedFontCJKMerge, customFontList, numFonts)
+            if success then
+                options.fontNameCJKMerge = customFontList[selectedFontCJKMerge]
+            end		
+            imgui.PopItemWidth()
+
+            imgui.PushItemWidth(imgui.GetWindowWidth() * 0.25)
+            success, fontSizeCJKMerge = imgui.InputFloat("Custom Font Size (CJK Range)", options.fontSizeCJKMerge)
+            if success then
+                options.fontSizeCJKMerge = RestrictOptionsRange(fontSizeCJKMerge, MIN_FONT_SIZE, MAX_FONT_SIZE)
+            end
+            imgui.PopItemWidth()
+
+            imgui.TreePop()
+        end
+        
+        -- Horizontal oversampling
+        imgui.PushItemWidth(imgui.GetWindowWidth() * 0.25)
+        success, oversampleH = imgui.InputInt("Custom Font Oversampling (Horizontal)", options.oversampleH)
+        if success then
+            options.oversampleH = RestrictOptionsRange(oversampleH, 1, 7)
+        end
+        imgui.PopItemWidth()
+        
+        -- Imgui doesn't support vertical oversampling on fonts yet
+        --imgui.PushItemWidth(imgui.GetWindowWidth() * 0.25)
+        --success, oversampleV = imgui.InputInt("Custom Font Oversampling (Vertical)", options.oversampleV)
+        --if success then
+            --options.oversampleV = RestrictOptionsRange(oversampleV, 1, 7)
+        --end
+        --imgui.PopItemWidth()
+
         imgui.TreePop()
     end
 end
 
+-- Settings specific to this addon
 local function PresentAddonSettings()
     local success
-    local refresh 
-        
+    
     if imgui.TreeNodeEx("This Addon's Configuration", "") then
-        imgui.PushItemWidth(imgui.GetWindowWidth() * 0.25)
-        success, refresh = imgui.InputInt("Font Change Delay (Seconds)", options.refreshTimer)
-        if success and refresh >= 0 and refresh <= 10 then
-            options.refreshTimer = refresh
-            addonOptionsChanged = true
-            globalOptionsChanged = true -- didn't change but we might be in  timer already
-        end
-        imgui.PopItemWidth()
-                
         if imgui.Checkbox("No title bar", options.noTitleBar == "NoTitleBar") then
             if options.noTitleBar == "NoTitleBar" then
                 options.noTitleBar = ""
             else
                 options.noTitleBar = "NoTitleBar"
             end
-            addonOptionsChanged = true
         end
-                        
+            
         if imgui.Checkbox("Transparent Window?", options.transparentWindow) then
             options.transparentWindow = not options.transparentWindow
-            addonOptionsChanged = true
         end
-                        
+            
         if imgui.Checkbox("No Resize?", options.noResize == "NoResize") then
             if options.noResize == "NoResize" then
                 options.noResize = ""
             else 
                 options.noResize = "NoResize"
             end
-            addonOptionsChanged = true
         end
-                        
+            
         if imgui.Checkbox("No Move?", options.noMove == "NoMove") then
             if options.noMove == "NoMove" then
                 options.noMove = ""
             else 
                 options.noMove = "NoMove"
             end
-            addonOptionsChanged = true
         end
         imgui.TreePop()
     end
 end
 
+local function PresentSaveButton()
+    if imgui.Button("Save") then
+        SaveThisAddonsOptions()
+        ChangeGlobalFont()
+    end
+end
+
+-- Present options/configuration window (this addon itself)
 local function PresentSettingsWindow()
     PresentGlobalSettings()
     PresentAddonSettings()	
+    PresentSaveButton()
+end
+
+-- Setup the options table 
+if optionsLoaded then
+    for _, opt in pairs(_SettingsEditorDefaults) do
+        options[opt[1]] = lib_helpers.NotNilOrDefault(options[opt[1]], opt[2])
+    end
+    pso.set_language(options.language)
+else
+    options = {}
+    for _, opt in pairs(_SettingsEditorDefaults) do
+        options[opt[1]] = opt[2]
+    end
+    
+    -- We just created the options, so we should save to have valid file
+    SaveOptions(options, optionsFileName) 
 end
 
 -- Load font list, set the global font because it's saved here
 LoadFontList()
 ChangeGlobalFont()
 
--- Frame timer to prevent every single input change holding up the client
-local saveCounter = 0
-local function CheckSaveOptionsCounter()
-    if saveCounter > 0 then
-        saveCounter = saveCounter - 1
-        if saveCounter == 0 then
-            SaveOptions(options, optionsFileName)
-            ChangeGlobalFont()
-        end
-    end
-end
-
--- Start the timer in # frames
-local function StartSaveOptionsCounter()
-    saveCounter = options.refreshTimer * 30
-end
-
 -- Called from the plugin to display the addon
 local function present()
-    CheckSaveOptionsCounter()
     if not display then
         return
     end
-        
-    if addonOptionsChanged then
-        -- Always save these immediately
-        SaveOptions(options, optionsFileName)
-    end
-        
-    if globalOptionsChanged then
-        -- These take a while to change, so let the user wait a second
-        -- in case they were typing out something like "20" for fontSize.
-        -- Really don't want the addon to immediately change font to 
-        -- fontSize 2 and make the user wait before they can finish typing 20.
-        StartSaveOptionsCounter()
-    end
-
-    globalOptionsChanged = false
-    addonOptionsChanged = false
-        
+    
     if options.transparentWindow == true then
         imgui.PushStyleColor("WindowBg", 0.0, 0.0, 0.0, 0.0)
     end
-        
+    
     local success
     success, display = imgui.Begin("Settings Editor", display, {options.noTitleBar, options.noResize, options.noMove})
     imgui.SetWindowFontScale(options.fontScale)
 
     PresentSettingsWindow()
-        
+    
     imgui.End()
 end
 
@@ -295,7 +322,7 @@ end
 
 local function init()
     core_mainmenu.add_button("Settings Editor", mainMenuButtonHandler)
-        
+    
     return 
     {
         name = 'Settings Editor',
@@ -303,6 +330,9 @@ local function init()
         author = 'Ender',
         present = present,
         toggleable = true,
+        MIN_FONT_SIZE = MIN_FONT_SIZE,
+        MAX_FONT_SIZE = MAX_FONT_SIZE,
+        SetAddonLanguage = SetAddonLanguage,
     }
 end
 
